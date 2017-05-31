3b235799c59156b8773c3e2a0fe81be5ef394c13
Kenneth Vo <kennyvo91@gmail.com>
2017-05-31 09:05:54 -0500

    cleaned up comments

--- config/routes.js
+++ config/routes.js
@@ -1,7 +1,7 @@
 var express = require('express'),
     router = express.Router(),
-    bodyParser = require('body-parser'), //parses information from POST
-    methodOverride = require('method-override'), //used to manipulate POST
+    bodyParser = require('body-parser'),
+    methodOverride = require('method-override'),
     passport = require("passport");
 
 var restaurantsController = require('../controllers/restaurants');
@@ -9,11 +9,9 @@ var usersController   = require('../controllers/users');
 
 var isAuthenticated = function (req, res, next) {
   // if user is authenticated in the session, call the next() to call the next request handler
-  // Passport adds this method to request object. A middleware is allowed to add properties to
-  // request and response objects
   if (req.isAuthenticated())
     return next();
-  // if the user is not authenticated then redirect him to the login page
+  // if the user is not authenticated then redirect to login page
   res.redirect('/');
 }
 
