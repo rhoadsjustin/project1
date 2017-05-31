140306100ac2c4ae2d560a7ccdad45d7d5238ca9
Justin Rhoads <rhoadsjustin@gmail.com>
2017-05-31 09:56:55 -0500

    added facebook auth route

--- config/routes.js
+++ config/routes.js
@@ -1,12 +1,27 @@
 var express = require('express'),
     router = express.Router(),
     bodyParser = require('body-parser'), //parses information from POST
-    methodOverride = require('method-override'); //used to manipulate POST
+    methodOverride = require('method-override'), //used to manipulate POST
+    passport = require('passport');
 
 var restaurantsController = require('../controllers/restaurants.js');
+var userController = require('../controllers/users');
+
+var isAuthenticated = function (req, res, next) {
+  // if user is authenticated in the session, call the next() to call the next request handler
+  if (req.isAuthenticated())
+    return next();
+  // if the user is not authenticated then redirect to login page
+  res.redirect('/');
+}
 
 router.route('/yelpapi/:term')
 
   .get(restaurantsController.searchYelp);
+
+router.route('/login/facebook').get( usersController.getLogin);
+router.route('/login/facebook/callback').get(usersController.getCallback);
+router.route('/logout').get(usersController.logout);
+
 
 module.exports = router;
